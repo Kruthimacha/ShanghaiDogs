db3de9806bbd7da04e611107da5cfaeaf4e14822
Kruthi Rao <82725469+Kruthimacha@users.noreply.github.com>
2026-03-30 18:34:05 +1000

    Add Data_Download.sh for downloading external cohorts SPIRE study data

---
+++ resource_generation/One_Health/Data_Download.sh
@@ -0,0 +1,7 @@
+spire --study download mags Coelho_2018_dog -o Coelho_2018_dog/
+spire --study download mags Wang_2019_dogs -o Wang_2019_dogs/
+spire --study download mags Yarlagadda_2022_global_dog -o Yarlagadda_2022_global_dog/
+spire --study download mags Allaway_2020_dogs -o Allaway_2020_dogs/
+spire --study download mags Liu_2021_Canidae -o Liu_2021_Canidae/
+spire --study download mags Xu_2019_dogs -o Xu_2019_dogs/
+spire --study download mags Worsley-Tonks_2020_dog -o Worsley-Tonks_2020_dog/
